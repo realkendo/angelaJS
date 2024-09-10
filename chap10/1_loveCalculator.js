a866c841ce0f7845a46550f8eb136197d4fd052e
realkendo <kennethistifanus1@gmail.com>
2024-09-10 11:10:50 +0100

    functionality modification

--- chap10/1_loveCalculator.js
+++ chap10/1_loveCalculator.js
@@ -9,15 +9,14 @@ window.onload = () => {
     let inputField = document.getElementById("inputField");
     inputField.style.height = "30vh";
 
+    //creating an input element for -1
     //input -1
     const input1 = document.createElement("input");
     input1.setAttribute("type", "text");
     input1.setAttribute("id", "name1");
     input1.classList.add("inputs");
     input1.setAttribute("placeholder", "enter name of first lover");
-
-    //creating an input element for -1
-    //input -1
+    //input -2
     const input2 = document.createElement("input");
     input2.setAttribute("type", "text");
     input2.setAttribute("id", "name2");
@@ -29,12 +28,23 @@ window.onload = () => {
     resultBtn.classList.add("calBtn");
     resultBtn.innerHTML = "Calculate";
 
+    // appending to parent elements
     inputField.appendChild(input1);
     inputField.appendChild(input2);
     inputField.appendChild(resultBtn);
 
+    // click event listener
     resultBtn.addEventListener("click", () => {
-      alert("click 'OK' to proceed");
+      let resultVariable = document.createElement("div");
+      let resultValue = document.createElement("h4");
+      let resultField = document.getElementById("resultField");
+
+      // adding attributes
+
+      resultValue.innerHTML = 4;
+
+      resultVariable.appendChild(resultValue);
+      resultField.appendChild(resultVariable);
     });
   });
 };
